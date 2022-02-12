fcfe2feaddb3b2436e64fe8fd49474ddd6458263
janson <janson@gmail.com>
2022-02-12 22:11:08 +0800

    aria2 module

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# rogsoft_aria2
-aria2 plugin for rogsoft merlin
+# merlin_ddnsto
+
+koolshare小宝开发的基于http2的远程控制，仅支持远程管理路由器+nas+windows远程桌面。
---
+++ config.json.js
@@ -0,0 +1,11 @@
+{
+    "author": "sadog", 
+    "description": "linux下载利器", 
+    "home_url": "Module_aria2.asp", 
+    "link": "", 
+    "md5": "c72c1519b5ef34e4fc67b17c533d5a28", 
+    "module": "aria2", 
+    "tags": "下载 USB", 
+    "title": "aria2", 
+    "version": "2.2"
+}
