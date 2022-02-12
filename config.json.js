b3a4d16152391615217d570603a231172df182bc
janson <janson@gmail.com>
2022-02-12 22:35:51 +0800

    version 2.3.1

--- config.json.js
+++ config.json.js
@@ -3,9 +3,9 @@
     "description": "linux下载利器", 
     "home_url": "Module_aria2.asp", 
     "link": "", 
-    "md5": "c72c1519b5ef34e4fc67b17c533d5a28", 
+    "md5": "c11f53f9e1b7b1f8ba80c96897c391e4", 
     "module": "aria2", 
     "tags": "下载 USB", 
     "title": "aria2", 
-    "version": "2.3"
+    "version": "2.3.1"
 }
